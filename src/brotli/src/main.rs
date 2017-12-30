659e66d7a9441bf0d9ae0d7f9e84d7a6fa626969
Dylan Frankland <dylan@frankland.io>
2017-12-29 22:15:30 -0800

    Clippy lint fix

--- src/brotli/src/main.rs
+++ src/brotli/src/main.rs
@@ -17,7 +17,7 @@ pub extern "C" fn alloc(size: usize) -> *mut c_void {
     let mut buf = Vec::with_capacity(size);
     let ptr = buf.as_mut_ptr();
     mem::forget(buf); // This is JS' responsibility now
-    return ptr as *mut c_void;
+    ptr as *mut c_void
 }
 
 // Actual module code
