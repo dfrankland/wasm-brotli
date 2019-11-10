70c5c5fc2ba15f2d63ca853bb76d9bfccf31e858
Dylan Frankland <dfrankland@users.noreply.github.com>
2019-11-09 22:19:22 -0800

    Remove files array from published package.json

--- update-package-json.ts
+++ update-package-json.ts
@@ -4,6 +4,8 @@ import packageJson from './pkg/package.json';
 
 packageJson.browser = 'wasm_brotli_browser.js';
 
+Object.defineProperty(packageJson, 'files', { value: undefined });
+
 writeFileSync(
   resolvePath(__dirname, './pkg/package.json'),
   JSON.stringify(packageJson, null, '  '),
